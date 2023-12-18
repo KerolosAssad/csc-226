6ed241929083a935e1c4bbe471547f8ecd7b490f
KerolosAssad <143562491+KerolosAssad@users.noreply.github.com>
2023-12-18 13:54:34 -0500

    Latest version

--- Dockerize-PHP-Application/app/api/newApi.php
+++ Dockerize-PHP-Application/app/api/newApi.php
@@ -1,22 +1,19 @@
 <?php
 
+// Allow requests from any origin
+header('Access-Control-Allow-Origin: *');
+
+// Allow these methods from any origin
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
+// Allow these headers from any origin
+header('Access-Control-Allow-Headers: Content-Type');
+
+// Set content type to JSON
+header('Content-Type: application/json; charset=UTF-8');
+
 // URL of the REST API endpoint for Fruityvice
 $api_url = 'https://www.fruityvice.com/api/fruit/all';
-
-// Data to send in the POST request (in JSON format)
-$post_data = array(
-    'key1' => 'value1',
-    'key2' => 'value2'
-);
-
-// Encode the POST data as JSON
-$json_post_data = json_encode($post_data);
-
-// Headers for the request (including the Content-Type header for JSON)
-$headers = array(
-    'Content-Type: application/json', // Set the content type to JSON
-    //No Authorization header for this API
-);
 
 // Initialize cURL session
 $ch = curl_init();
@@ -24,52 +21,28 @@ $ch = curl_init();
 // Set cURL options for GET request
 curl_setopt($ch, CURLOPT_URL, $api_url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
 // Execute the GET request
 $response_get = curl_exec($ch);
 
 if ($response_get === false) {
-    echo 'cURL GET request error: ' . curl_error($ch);
+    echo json_encode(['error' => 'cURL GET request error: ' . curl_error($ch)]);
 } else {
     // Parse the JSON response
-    $data_get = json_decode($response_get, true); // Set the second argument to true to get an associative array
+    $data_get = json_decode($response_get, true);
 
     // Handle the parsed data
     if ($data_get !== null) {
-        // $data_get now contains the JSON response as an array
-        var_dump($data_get);
+        // Output the JSON response
+        echo json_encode($data_get);
     } else {
-        echo 'Error parsing JSON response.';
-    }
-}
-
-// Reset cURL options for POST request
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $json_post_data); // Send JSON data
-
-// Set headers for the POST request
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-// Execute the POST request
-$response_post = curl_exec($ch);
-
-if ($response_post === false) {
-    echo 'cURL POST request error: ' . curl_error($ch);
-} else {
-    // Parse the JSON response
-    $data_post = json_decode($response_post, true); // Set the second argument to true to get an associative array
-
-    // Handle the parsed data
-    if ($data_post !== null) {
-        // $data_post now contains the JSON response as an array
-        var_dump($data_post);
-    } else {
-        echo 'Error parsing JSON response.';
+        echo json_encode(['error' => 'Error parsing JSON response.']);
     }
 }
 
 // Close cURL session
 curl_close($ch);
+
 ?>
+
 
--- Dockerize-PHP-Application/frontend/react-docker/src/components/Testcomponent.js
+++ Dockerize-PHP-Application/frontend/react-docker/src/components/Testcomponent.js
@@ -1,15 +1,112 @@
 import React, { useState, useEffect } from 'react';
 
 export default function MyComponent() {
-    const [data, setData] = useState(null);
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [filteredData, setFilteredData] = useState([]);
+    const [sortField, setSortField] = useState('calories');
+    const [sortOrder, setSortOrder] = useState('asc'); // 'asc' for ascending, 'desc' for descending
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
+        fetch('http://localhost:80/api/newApi.php')
             .then(response => response.json())
-            .then(data => setData(data));
+            .then(data => {
+                const formattedData = data.map(item => ({
+                    name: item.name,
+                    family: item.family,
+                    order: item.order,
+                    genus: item.genus,
+                    nutritions: item.nutritions
+                }));
+
+                setData(formattedData);
+                setFilteredData(formattedData);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+                setLoading(false);
+            });
     }, []);
 
+    const handleSearch = query => {
+        setSearchQuery(query);
+        const filtered = data.filter(item =>
+            item.name.toLowerCase().includes(query.toLowerCase())
+        );
+        setFilteredData(filtered);
+    };
 
-    // render the component using the data from the API
-    return <div>{JSON.stringify(data)}</div>;
+    const handleSort = () => {
+        const sortedData = [...filteredData].sort((a, b) => {
+            const valueA = a.nutritions[sortField];
+            const valueB = b.nutritions[sortField];
+
+            if (sortOrder === 'asc') {
+                return valueA - valueB;
+            } else {
+                return valueB - valueA;
+            }
+        });
+
+        setFilteredData(sortedData);
+    };
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    const displayData = filteredData.length ? filteredData : null;
+
+    return (
+        <div>
+            <div className="SearchBox">
+                <h2>Search Fruits:</h2>
+                <input
+                    type="text"
+                    value={searchQuery}
+                    onChange={e => handleSearch(e.target.value)}
+                />
+            </div>
+            <div className={"SortOrderDropdown"}>
+                <h2>Sort Order:</h2>
+                <select value={sortField} onChange={e => setSortField(e.target.value)}>
+                    <option value="calories">Calories</option>
+                    <option value="fat">Fat</option>
+                    <option value="sugar">Sugar</option>
+                    <option value="carbohydrates">Carbohydrates</option>
+                    <option value="protein">Protein</option>
+                </select>
+                <select value={sortOrder} onChange={e => setSortOrder(e.target.value)}>
+                    <option value="asc">Low to High</option>
+                    <option value="desc">High to Low</option>
+                </select>
+                <button onClick={handleSort}>Sort</button>
+            </div>
+            <h2>Results:</h2>
+            <div className="Result">
+                {displayData ? (
+                    displayData.map(item => (
+                        <div key={item.name}>
+                            <div>{`Name: ${item.name}`}</div>
+                            <div>{`Family: ${item.family}`}</div>
+                            <div>{`Order: ${item.order}`}</div>
+                            <div>{`Genus: ${item.genus}`}</div>
+                            <div><br/></div>
+                            <div>{`Nutritional Information:`}</div>
+                            <div>{`  Calories: ${item.nutritions.calories}`}</div>
+                            <div>{`  Fat: ${item.nutritions.fat}`}</div>
+                            <div>{`  Sugar: ${item.nutritions.sugar}`}</div>
+                            <div>{`  Carbohydrates: ${item.nutritions.carbohydrates}`}</div>
+                            <div>{`  Protein: ${item.nutritions.protein}`}</div>
+                            <hr />
+                        </div>
+                    ))
+                ) : (
+                    <div>No matching results found</div>
+                )}
+            </div>
+        </div>
+    );
 }
